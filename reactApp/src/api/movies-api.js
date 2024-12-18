07e8a8d05da6e5c840caf857a0cb1b622e225721
ahanasmeegg <1527926614@qq.com>
2024-12-18 11:07:03 +0800

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=37c96bbe42a6e98a056dc60f73cb4dad&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
 
 export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
