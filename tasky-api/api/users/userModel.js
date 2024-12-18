ef376e8114cfbf1cc6b84df8623ad55d1b2e5500
ahanasmeegg <1527926614@qq.com>
2024-12-18 08:44:17 +0800

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,7 +4,15 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: { type: String,
+             required: true,
+             validate: {
+              validator: (value) => {
+            return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(value);
+    },
+    message: 'Password must be at least 8 characters long, contain at least one letter, one number, and one special character.', 
+      },
+    },
 });
 
 export default mongoose.model('User', UserSchema);
