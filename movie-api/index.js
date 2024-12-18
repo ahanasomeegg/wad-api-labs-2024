49fd5942c1b3cbc811f65b36da1531542326172c
ahanasmeegg <1527926614@qq.com>
2024-12-18 10:39:41 +0800

    Added Auth Middleware

--- movie-api/index.js
+++ movie-api/index.js
@@ -5,6 +5,8 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
 import moviesRouter from './api/movies';
+import authenticate from './authenticate';
+
 
 dotenv.config();
 
@@ -14,7 +16,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter); 
+app.use('/api/movies', authenticate, moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
